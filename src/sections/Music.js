5ebadee89817fbbdc29c42f2cd085ffc838dc07c
Zhi Han <zhihuahan72@gmail.com>
2022-03-10 23:02:03 -0800

    Update Music.js

--- src/sections/Music.js
+++ src/sections/Music.js
@@ -22,7 +22,7 @@ const Music = () => {
       />
 
       <iframe
-        src="https://open.spotify.com/embed/playlist/58Kg5IirKtASXLYosizqnm"
+        src="https://open.spotify.com/embed/playlist/2DSFPIlaQLupu80jKp0dh9?"
         width="100%"
         height="500"
         frameBorder="0"
