ec3cb821dd62d033cb96e50ed21dc70b92c79183
Zhi Han <zhihuahan72@gmail.com>
2022-03-10 21:20:54 -0800

    just remove eslint lmao

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,6 @@
 const path = require(`path`);
 require(`dotenv`).config({ path: `.env` });
 
-// Get paths of Gatsby's required rules, which as of writing is located at:
-// https://github.com/gatsbyjs/gatsby/tree/fbfe3f63dec23d279a27b54b4057dd611dce74bb/packages/
-// gatsby/src/utils/eslint-rules
-const gatsbyRequiredRules = path.join(
-  process.cwd(),
-  "node_modules",
-  "gatsby",
-  "dist",
-  "utils",
-  "eslint-rules",
-);
-
 module.exports = {
   siteMetadata: {
     title: `Amruth Pillai's Resume on the Web`,
@@ -108,19 +96,6 @@ module.exports = {
       resolve: `gatsby-source-instagram-all`,
       options: {
         access_token: process.env.INSTAGRAM_TOKEN,
-      },
-    },
-    {
-      resolve: "gatsby-plugin-eslint",
-      options: {
-        // Gatsby required rules directory
-        rulePaths: [gatsbyRequiredRules],
-        // Default settings that may be ommitted or customized
-        stages: ["develop"],
-        extensions: ["js", "jsx", "ts", "tsx"],
-        exclude: ["node_modules", "bower_components", ".cache", "public"],
-        // Any additional eslint-webpack-plugin options below
-        // ...
       },
     },
   ],
