c26832d6a4a70c396599fda83514ef7fd06fedea
Zhi Han <zhihuahan72@gmail.com>
2022-05-11 10:21:28 -0700

    add formspark

--- src/sections/Contact.js
+++ src/sections/Contact.js
@@ -16,10 +16,11 @@ const Contact = () => {
 
   const [buttonText, setButtonText] = useState("Send Message");
 
-  const [submit, isSubmitting] = useFormspark({ formId: "LVr3mgSu" });
+  const [submit, isSubmitting] = useFormspark({ formId: "KC1gMEaj" });
 
   const isEmailValid = email => {
-    var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    var re =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
   };
 
